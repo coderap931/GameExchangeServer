aea46a7fb9dbefe65fb17bc1787926d17eb47a7f
Adam Patrick <coderap931@gmail.com>
2022-02-08 18:19:24 -0500

    changed db.js and .env

--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require('sequelize');
 
 //!For Local DB, change for deployment
-// const db = new Sequelize(`postgres://postgres:${process.env.DATABASE_PASSWORD}@localhost:5432/gameExchange`,{
+// const db = new Sequelize(`postgresql://postgres:${process.env.DATABASE_PASSWORD}@localhost:5432/gameExchange`,{
 //     dialect: 'postgres',
 // });
 
 //!For Deployment, uncomment this and comment out Local DB connection
-const db = new Sequelize(`postgres://postgres:${encodeURIComponent(process.env.PASS)}@localhost/gameExchange`, {
+const db = new Sequelize(`postgresql://${process.env.USER}:${process.env.PASS}@${process.env.HOST}/${process.env.DBNAME}`, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
